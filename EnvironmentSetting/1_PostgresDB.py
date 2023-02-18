9fb14d620929469b8135952bef8d8cab8517b15c
peiyuwu <ts01174755@gmail.com>
2023-02-18 22:14:20 +0800

    0.0.2 MLOPS Start

--- EnvironmentSetting/1_PostgresDB.py
+++ EnvironmentSetting/1_PostgresDB.py
@@ -10,8 +10,8 @@ import os
 ############################################## 拉postgres + PgAdmin4 映像檔 ##############################################
 # 先 pull image
 os.system("docker pull postgres")
-os.system("docker pull dpage/pgadmin4") # 先 pull image
-os.system('docker images') # 查看所有 image
+os.system("docker pull dpage/pgadmin4")  # 先 pull image
+os.system('docker images')  # 查看所有 image
 
 ############################################## 建立container ##############################################
 # 建立 postgres container，設定 port 號，設定帳號與密碼
@@ -31,22 +31,28 @@ os.system(
     '"PGADMIN_DEFAULT_EMAIL=pgadmin4@gmail.com" '
     '-e "PGADMIN_DEFAULT_PASSWORD=pgadmin4" '
     '-d dpage/pgadmin4'
-) # 建立 container，設定 port 號，設定 pgadmin4 最高權限的登入密碼
+)  # 建立 container，設定 port 號，設定 pgadmin4 最高權限的登入密碼
 
 ############################################## 進入container ##############################################
-os.system('docker exec -i postgresdb apt-get update') # 更新 apt-get
-os.system('docker exec -i postgresdb apt-get install -y git') # 安裝 git
-os.system('docker exec -i postgresdb bash -c "apt-get install make"') # 安裝 make
-os.system('docker exec -i postgresdb bash -c "apt-get install gcc -y"') # 安裝 gcc
-os.system('docker exec -i postgresdb bash -c "apt-get install postgresql-contrib -y"') # 安裝 postgresql-contrib
-os.system('docker exec -i postgresdb bash -c "apt-get install postgresql-server-dev-15 -y"') # 安裝 postgresql-server-dev-15
-os.system('docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib && git clone https://github.com/pgpartman/pg_partman.git"') # 下載 pg_partman, https://github.com/pgpartman/pg_partman
-os.system('docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib/pg_partman && make install"') # 安裝 pg_partman, https://dbastreet.com/?p=1447
-os.system('docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib/pg_partman && make installcheck"') # 測試 pg_partman
+os.system('docker exec -i postgresdb apt-get update')  # 更新 apt-get
+os.system('docker exec -i postgresdb apt-get install -y git')  # 安裝 git
+os.system('docker exec -i postgresdb bash -c "apt-get install make"')  # 安裝 make
+os.system('docker exec -i postgresdb bash -c "apt-get install gcc -y"')  # 安裝 gcc
+os.system('docker exec -i postgresdb bash -c "apt-get install postgresql-contrib -y"')  # 安裝 postgresql-contrib
+os.system(
+    'docker exec -i postgresdb bash -c "apt-get install postgresql-server-dev-15 -y"')  # 安裝 postgresql-server-dev-15
+os.system(
+    'docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib && git clone https://github.com/pgpartman/pg_partman.git"')  # 下載 pg_partman, https://github.com/pgpartman/pg_partman
+os.system(
+    'docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib/pg_partman && make install"')  # 安裝 pg_partman, https://dbastreet.com/?p=1447
+os.system(
+    'docker exec -i postgresdb bash -c "cd usr/share/postgresql/15/contrib/pg_partman && make installcheck"')  # 測試 pg_partman
 
-############################################## 建立DataBase & Table With Partition##############################################
-os.system("docker exec -i postgresdb psql -U postgres -c \"create role postgresuser with login password 'postgresuser';\"") # 建立 postgresUser
-os.system('docker exec -i postgresdb psql -U postgres -c "create database postgresdbuser owner postgresuser"') # 建立 postgresdb1
+############################################## 建立DataBase & Table With Partition ##############################################
+os.system(
+    "docker exec -i postgresdb psql -U postgres -c \"create role postgresuser with login password 'postgresuser';\"")  # 建立 postgresUser
+os.system(
+    'docker exec -i postgresdb psql -U postgres -c "create database postgresdbuser owner postgresuser"')  # 建立 postgresdb1
 os.system('docker exec -i postgresdb psql -U postgres -d postgresdbuser -c "create schema userschema"')  # 建立 schema
 os.system(
     'docker exec -i postgresdb psql -U postgres -d postgresdbuser -c "\
@@ -64,16 +70,17 @@ os.system(
 )  # 建立 Table, https://docs.postgresql.tw/the-sql-language/ddl/table-partitioning#5.11.5.-partitioning-and-constraint-exclusion
 
 # 使用pg_partman建立分區表
-os.system('docker exec -i postgresdb psql -U postgres -d postgresdbuser -c "create extension pg_partman"') # 建立 pg_partman, chrome-extension://noogafoofpebimajpfpamcfhoaifemoa/suspended.html#ttl=%E4%BD%BF%E7%94%A8%20pg_partman%20%E6%93%B4%E5%85%85%E5%8A%9F%E8%83%BD%E4%BE%86%E7%AE%A1%E7%90%86%20PostgreSQL%20%E5%88%86%E5%89%B2%E5%8D%80%20-%20Amazon%20Relational%20Database%20Service&pos=0&uri=https://docs.aws.amazon.com/zh_tw/AmazonRDS/latest/UserGuide/PostgreSQL_Partitions.html#PostgreSQL_Partitions.enable
+os.system(
+    'docker exec -i postgresdb psql -U postgres -d postgresdbuser -c "create extension pg_partman"')  # 建立 pg_partman, chrome-extension://noogafoofpebimajpfpamcfhoaifemoa/suspended.html#ttl=%E4%BD%BF%E7%94%A8%20pg_partman%20%E6%93%B4%E5%85%85%E5%8A%9F%E8%83%BD%E4%BE%86%E7%AE%A1%E7%90%86%20PostgreSQL%20%E5%88%86%E5%89%B2%E5%8D%80%20-%20Amazon%20Relational%20Database%20Service&pos=0&uri=https://docs.aws.amazon.com/zh_tw/AmazonRDS/latest/UserGuide/PostgreSQL_Partitions.html#PostgreSQL_Partitions.enable
 os.system(
     'docker exec -i postgresdb psql -U postgres -d postgresdbuser -c \
     "select create_parent(\'userschema.userextract\', \'dt\', \'native\', \'daily\', p_start_partition := \'2021-01-01\')"\
     '
-) # 建立分區表
+)  # 建立分區表
 
 ############################################## PgAdmin4 Setting ##############################################
 # 連postgresdb
-os.system('docker inspect postgresdb') # 查看 container 資訊，找出Networks.bridge.IPAddress
+os.system('docker inspect postgresdb')  # 查看 container 資訊，找出Networks.bridge.IPAddress
 os.system('open http://localhost:5050/')
 # >> 點擊 "Add Server"
 # >> Name: postgresdb
@@ -84,10 +91,12 @@ os.system('open http://localhost:5050/')
 # >> Password: postgres
 
 # 下一步：建立DataBase    [V]
-# 下一步：建立Table       [X]
-# 下一步：建立Table欄位    [X]
-# 下一步：建立Table欄位的資料型態 [X]
+# 下一步：建立Table       [V]
+# 下一步：建立Table欄位    [V]
+# 下一步：建立Table欄位的資料型態 [V]
+# 下一步：建立分區表         [V]
 # 下一步：建立測試資料        [X]
+# 下一步：測試分區成功        [X]
 
 ############################################## PostgreSQL 指令 ##############################################
 # 建立 postgres DataBase
@@ -105,6 +114,3 @@ os.system('open http://localhost:5050/')
 # os.system('docker exec -i postgresdb psql -U postgres -d postgresdbtest -c "alter table userextract drop column age"') # 刪除 Table欄位
 # os.system('docker exec -i postgresdb psql -U postgres -d postgresdbtest -c "drop table userextract"') # 刪除 Table
 # os.system('docker exec -i postgresdb psql -U postgres -c "drop database postgresdbtest"') # 刪除 DataBase
-
-
-
