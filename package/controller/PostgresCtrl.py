28a7a29f6e40f82d1018ebe1c0f1aaaea4ce587b
peiyuwu <ts01174755@gmail.com>
2023-03-11 12:12:52 +0800

    0.2.1 MLOPS 確定MLOPS架構 - package/environment 架構完成

---
+++ package/controller/PostgresCtrl.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+class PostgresCtrl:
+    def __init__(self, host, database, user, password):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.conn = None
+
+    def connect(self):
+        if self.conn is None:
+            self.conn = psycopg2.connect(
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password
+            )
+        return self.conn # 這裡要回傳conn, 不然會出現NoneType has no attribute 'cursor'的錯誤
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+    def execute(self, query): # 執行資料庫指令
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        self.conn.commit() # 這裡要記得commit, 不然資料庫不會更新, 這是一個很常犯的錯誤
+        cursor.close()
+
+    def query(self, query): # 查詢資料
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+
