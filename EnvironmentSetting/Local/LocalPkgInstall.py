47acdf6d5dacd5493b74e53072863a2e0590c934
peiyuwu <ts01174755@gmail.com>
2023-03-12 19:30:11 +0800

    0.3.1 MLOPS GoogleFormApi.py

--- EnvironmentSetting/Local/LocalPkgInstall.py
+++ EnvironmentSetting/Local/LocalPkgInstall.py
@@ -8,3 +8,11 @@ subprocess.run('pip install fastapi', shell=True)
 subprocess.run('pip install uvicorn', shell=True)
 subprocess.run('pip install requests', shell=True)
 subprocess.run('pip install beautifulsoup4', shell=True)
+
+# 安裝 google form api 相關的套件
+subprocess.run('pip install google-api-python-client', shell=True)
+subprocess.run('pip install google-auth-httplib2', shell=True)
+subprocess.run('pip install google-auth-oauthlib', shell=True)
+subprocess.run('pip install oauth2client', shell=True)
+
+
