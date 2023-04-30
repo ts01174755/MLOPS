f9fb70be35e57c54e960063b714cf533d5c2acd0
peiyuwu <ts01174755@gmail.com>
2023-04-30 20:21:13 +0800

    1.0.3 MLOPS - 微服務架構 - 環境建立

--- env_local_pkgInstall.py
+++ env_local_pkgInstall.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         subprocess.run("pip install --upgrade pip", shell=True)
         subprocess.run("brew install tree", shell=True)
 
-    elif RUN == 'base' or RUN == 'all':
+    if RUN == 'base' or RUN == 'all':
         PY_PKG_LIST = [
             'python-dotenv', 'psycopg2-binary', 'sqlalchemy', 'pymongo', 'fastapi', 'Jinja2==3.1.2',
             'uvicorn[standard]', 'aiofiles', 'requests', 'beautifulsoup4', 'pandas', 'black',
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         for pkg_ in PY_PKG_LIST:
             subprocess.run(f"pip install {pkg_}", shell=True)
 
-    elif RUN == 'google' or RUN == 'all':
+    if RUN == 'google' or RUN == 'all':
         # 安裝 google form api 相關的套件
         GOOGLE_PKG_LIST = [
             'google-api-python-client', 'google-auth', 'google-auth-httplib2', 'google-auth-oauthlib',
@@ -29,6 +29,6 @@ if __name__ == "__main__":
         for pkg_ in GOOGLE_PKG_LIST:
             subprocess.run(f"pip install {pkg_}", shell=True)
 
-    elif RUN == 'OTHER' or RUN == 'all':
+    if RUN == 'OTHER' or RUN == 'all':
         # 其他套件
         subprocess.run('brew install smartmontools', shell=True)
